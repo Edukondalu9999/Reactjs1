34281a71b6133b1705456fa8931ae3c9c5336555
Akanaboina Edukondalu <ea21027@corp.ojas-it.com>
2021-10-27 10:55:31 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionComp from './FunctionComp';
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionComp/>
+    </div>
+  );
+}
+
+export default App;
